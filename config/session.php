8e0adde6f66b073916831499a2a3714bc25e1d49
nadanada2 <153773260+nadanada2@users.noreply.github.com>
2025-11-29 22:48:36 +0100

    Add files via upload

---
+++ config/session.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+
+function isAdmin() {
+    return isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
+}
+
+function redirectIfNotLoggedIn() {
+    if (!isLoggedIn()) {
+        header("Location: login.php");
+        exit();
+    }
+}
+
+function redirectIfNotAdmin() {
+    if (!isAdmin()) {
+        header("Location: index.php");
+        exit();
+    }
+}
+?>
